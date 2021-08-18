11afcc6a3a559f5e523852c7bd32db85de152bee
kgm-930 <72342542+kgm-930@users.noreply.github.com>
2021-08-18 13:33:37 +0900

    0818
    updated screen

--- app/src/main/java/com/example/android/screen_1.java
+++ app/src/main/java/com/example/android/screen_1.java
@@ -3,16 +3,19 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.pm.ActivityInfoCompat;
-
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.MotionEvent;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class screen_1 extends AppCompatActivity {
 
-    private Button button_transaction;
+    private DrawerLayout drawerLayout;
+    private View drawerView;
     private long backbutton_TIME = 0;
 
     @Override
@@ -20,19 +23,52 @@ public class screen_1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen1);
 
-        button_transaction=findViewById(R.id.button_transaction);
+        drawerLayout = (DrawerLayout) findViewById(R.id.screen1);
+        drawerView = (View) findViewById(R.id.drawer);
 
-        button_transaction.setOnClickListener(new View.OnClickListener() {
+        ImageButton btn_open = (ImageButton) findViewById(R.id.icon_mymenu);
+
+        btn_open.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(screen_1.this, screen_2.class);
-                startActivity(intent);
+                drawerLayout.openDrawer(drawerView);
             }
         });
-    }
 
-    @Override
-    public void onBackPressed() {
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+//    네비게이션 드로어
+
+        DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        };
+    };
+//      네비게이션 드로어 속성
+
+    public void onBackPressed(){
         long cur_TIME = System.currentTimeMillis();
         long gap_TIME = cur_TIME - backbutton_TIME;
 
@@ -44,7 +80,8 @@ public class screen_1 extends AppCompatActivity {
 
         else{
             backbutton_TIME = cur_TIME;
-            Toast.makeText(this, "한 번 더 누를 시 종료합니다",Toast.LENGTH_SHORT).show();;
+            Toast.makeText(this, "한 번 더 누를 시 종료합니다",Toast.LENGTH_SHORT).show();
         }
     }
+//    뒤로가기 두번 버튼
 }
