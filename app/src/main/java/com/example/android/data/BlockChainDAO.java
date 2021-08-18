e406bd42212df16430041861198f5ba91965dabe
ZIrlrl <cjlrh9987@gmail.com>
2021-08-18 15:31:49 +0900

    0818-2
    최근거래내역 올라가고 내려가게
    transtale up
    transtale down

--- app/src/main/java/com/example/android/data/BlockChainDAO.java
+++ app/src/main/java/com/example/android/data/BlockChainDAO.java
@@ -1,38 +1,38 @@
-package com.example.android.data;
-
-import org.reactivestreams.Subscription;
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.DefaultBlockParameterName;
-import org.web3j.protocol.core.methods.response.EthAccounts;
-import org.web3j.protocol.core.methods.response.EthGetBalance;
-import org.web3j.protocol.core.methods.response.Web3ClientVersion;
-import org.web3j.protocol.http.HttpService;
-import org.web3j.utils.Convert;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-
-public class BlockChainDAO {
-    UserWallet userWallet = new UserWallet();
-
-    public BlockChainDAO(String address) throws Exception{
-        // Web3j 연결부분
-        Web3j web3j = Web3j.build(new HttpService("http://3.35.235.189:8547"));
-
-        // 잔액 조회 부분
-        EthGetBalance ethGetBalance = web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).sendAsync().get();
-        BigDecimal ether = Convert.fromWei(ethGetBalance.getBalance().toString(), Convert.Unit.ETHER);
-
-        // 거래 내역 부분
-        Subscription subscription = (Subscription) web3j.replayPastTransactionsFlowable(DefaultBlockParameterName.EARLIEST,DefaultBlockParameterName.LATEST).subscribe(System.out::println);
-
-        // UserWallet 생성 + 생성자 값 대입
-        UserWallet userWallet = new UserWallet(address, ether, history);
-        this.userWallet = userWallet;
-    }
-
-    public UserWallet getUserWallet() {
-        return userWallet;
-    }
-}
-
+//package com.example.android.data;
+//
+//import org.reactivestreams.Subscription;
+//import org.web3j.protocol.Web3j;
+//import org.web3j.protocol.core.DefaultBlockParameterName;
+//import org.web3j.protocol.core.methods.response.EthAccounts;
+//import org.web3j.protocol.core.methods.response.EthGetBalance;
+//import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+//import org.web3j.protocol.http.HttpService;
+//import org.web3j.utils.Convert;
+//
+//import java.math.BigDecimal;
+//import java.util.ArrayList;
+//
+//public class BlockChainDAO {
+//    UserWallet userWallet = new UserWallet();
+//
+//    public BlockChainDAO(String address) throws Exception{
+//        // Web3j 연결부분
+//        Web3j web3j = Web3j.build(new HttpService("http://3.35.235.189:8547"));
+//
+//        // 잔액 조회 부분
+//        EthGetBalance ethGetBalance = web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).sendAsync().get();
+//        BigDecimal ether = Convert.fromWei(ethGetBalance.getBalance().toString(), Convert.Unit.ETHER);
+//
+//        // 거래 내역 부분
+//        Subscription subscription = (Subscription) web3j.replayPastTransactionsFlowable(DefaultBlockParameterName.EARLIEST,DefaultBlockParameterName.LATEST).subscribe(System.out::println);
+//
+//        // UserWallet 생성 + 생성자 값 대입
+//        UserWallet userWallet = new UserWallet(address, ether, history);
+//        this.userWallet = userWallet;
+//    }
+//
+//    public UserWallet getUserWallet() {
+//        return userWallet;
+//    }
+//}
+//
--- app/src/main/java/com/example/android/screen_1.java
+++ app/src/main/java/com/example/android/screen_1.java
@@ -8,8 +8,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class screen_1 extends AppCompatActivity {
@@ -17,6 +20,12 @@ public class screen_1 extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private View drawerView;
     private long backbutton_TIME = 0;
+//q
+    boolean flag = true; //올라오고 내려온 상태
+    Animation transup; //올라오는 애니메이션
+    Animation transdown; //내려가는 애니메이션
+    LinearLayout bottomSheet; // 올라오고 내려갈 최근거래내역
+//q
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +74,51 @@ public class screen_1 extends AppCompatActivity {
 
             }
         };
+        //q
+        bottomSheet = findViewById(R.id.bottomSheet);
+        transup= AnimationUtils.loadAnimation(this,R.anim.translateup);
+        transdown=AnimationUtils.loadAnimation(this,R.anim.translatedown);
+        Sliding animationListener = new Sliding();
+        transup.setAnimationListener(animationListener);
+        transdown.setAnimationListener(animationListener);
+        //q
     };
+    //q
+    public void IB_clicked(View v) {
+        if (flag) {
+            bottomSheet.startAnimation(transup);
+        }
+        else {
+            bottomSheet.setVisibility(View.VISIBLE);
+            bottomSheet.startAnimation(transdown);
+        }
+    }
+//    public void IB_clicked2(View v) {
+//        if (!flag) {
+//            bottomSheet.setVisibility(View.VISIBLE);
+//            bottomSheet.startAnimation(transdown);
+//        }
+//    }
+    private class Sliding implements Animation.AnimationListener {
+        @Override
+        public void onAnimationStart(Animation animation) {
+            //애니메이션 시작할때 호출되는 메서드
+        }
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            //애니메이션 종료할때 호출되는 메서드
+            if (flag) {
+                flag = false;
+            }
+            else {
+                flag = true;
+            }
+        }
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+        }
+    }
+    //q
 //      네비게이션 드로어 속성
 
     public void onBackPressed(){
