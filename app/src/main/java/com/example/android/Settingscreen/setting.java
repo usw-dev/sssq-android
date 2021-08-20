2b768f9f714dd8da90ba90019ed584be6e3b7e77
kgm-930 <72342542+kgm-930@users.noreply.github.com>
2021-08-20 23:54:46 +0900

    0820
    appver

--- app/src/main/java/com/example/android/Settingscreen/setting.java
+++ app/src/main/java/com/example/android/Settingscreen/setting.java
@@ -2,15 +2,31 @@ package com.example.android.Settingscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.android.R;
+import com.example.android.ScreenVerActivity;
 
 public class setting extends AppCompatActivity {
+
+    private Button button_ver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_setting);
+
+        button_ver = findViewById(R.id.app_ver);
+
+        button_ver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(setting.this, ScreenVerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
